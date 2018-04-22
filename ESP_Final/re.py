877d7dc835189b4fdb4e701cdc78600c7d0ea799
Hanmant lokare <35780694+Hanmantlokare@users.noreply.github.com>
2018-04-21 23:03:02 -0400

    Add files via upload

---
+++ ESP_Final/re.py
@@ -0,0 +1,24 @@
+
+import sys,fileinput
+
+
+def main():
+	
+    file = sys.argv[1]
+
+    f = open(file, 'r')
+    filedata = f.read()
+    f.close()
+   
+    f = open('public_IP.txt','r')
+    data = f.read()
+    f.close()
+
+    newdata = filedata.replace("xxxx", data)
+
+    f = open(file, 'w')
+    f.write(newdata)
+    f.close()
+
+if __name__ == "__main__":
+     main()
